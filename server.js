c9ac5d90309dde4d6ece0f9329f57d15623495a7
Jake Rhoads <jake.rhoads@gmail.com>
2020-07-02 20:35:45 -0400

    additional note about note add and delete issues plus heroku launched

--- server.js
+++ server.js
@@ -17,3 +17,7 @@ app.listen(PORT, function () {
 });
 
 // Notes will not save or delete unless I kill the node and restart the server
+// So, in the app launched in Heroku, there are no notes saving or able to be deleted,
+// however, when run just through localhost with node server.js, notes
+// are saving and able to be deleted, but the server has to be killed
+// and restarted in order to see the updated or deleted notes.
