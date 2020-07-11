f52987a133024aadfce2e645d01ef9f82957bd93
Jake Rhoads <jake.rhoads@gmail.com>
2020-07-11 17:22:01 -0400

    removed uneeded comments for missing code

--- server.js
+++ server.js
@@ -16,8 +16,3 @@ app.listen(PORT, function () {
   console.log("App listening on PORT: " + PORT);
 });
 
-// Notes will not save or delete unless I kill the node and restart the server
-// So, in the app launched in Heroku, there are no notes saving or able to be deleted,
-// however, when run just through localhost with node server.js, notes
-// are saving and able to be deleted, but the server has to be killed
-// and restarted in order to see the updated or deleted notes.
